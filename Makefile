de4aa977ba6ac1ce686cd83a8138ae4818332aa6
s-chepurnov <chepurnov.serge@gmail.com>
2022-02-24 22:11:14 +0700

    picocli init

---
+++ Makefile
@@ -0,0 +1,13 @@
+install:
+	./gradlew clean install
+
+run-dist:
+	./build/install/app/bin/app
+
+check-updates:
+	./gradlew dependencyUpdates
+
+build:
+	./gradlew clean build
+
+.PHONY: build
--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,21 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+class App implements Callable<Integer> {
+
+    @Override
+    public Integer call() throws Exception {
+        // your business logic goes here...
+        return 0;
+    }
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
