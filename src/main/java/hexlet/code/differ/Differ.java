cfd2019815f4a54000f3c2ba4235d6d95e5c6f57
s-chepurnov <chepurnov.serge@gmail.com>
2022-03-03 16:06:48 +0700

    5. Сравнение плоских файлов (JSON)

---
+++ src/main/java/hexlet/code/differ/Differ.java
@@ -0,0 +1,57 @@
+package hexlet.code.differ;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.*;
+
+public class Differ {
+
+    public static String generate(String content1, String content2) throws Exception {
+        Map<String, Object> map1 = getData(content1);
+        Map<String, Object> map2 = getData(content2);
+
+        //sort and make unique
+        Set<String> set = new TreeSet<>(map1.keySet());
+        Set<String> set2 = new TreeSet<>(map2.keySet());
+        set.addAll(set2);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("{\n");
+
+        for (String key : set) {
+            Object v1 = map1.get(key);
+            Object v2 = map2.get(key);
+
+            if (v1 != null && v2 == null) {
+                //- deleted
+                sb.append("  - " + key + ": " + v1);
+            } else if (v1 != null && v2 != null && v1.equals(v2)) {
+                //no change
+                sb.append("    " + key + ": " + v1);
+            } else if (v1 != null && v2 != null && !v1.equals(v2)) {
+                //- change
+                sb.append("  - " + key + ": " + v1);
+                sb.append("\n");
+                sb.append("  + " + key + ": " + v2);
+            } else if (v1 == null & v2 != null) {
+                //add
+                sb.append("  + " + key + ": " + v2);
+            }
+            sb.append("\n");
+        }
+
+        sb.append("}");
+        return sb.toString();
+    }
+
+    public static Map<String, Object> getData(String content) throws Exception {
+        return parse(content);
+    }
+
+    public static Map<String, Object> parse(String content) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> map = objectMapper.readValue(content, new TypeReference<Map<String,Object>>(){});
+        return map;
+    }
+}
